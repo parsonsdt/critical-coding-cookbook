82e097e16de26771f711e01d00c1f0fe655cf5ba
lizard98 <payde097@newschool.edu>
2021-06-07 19:08:12 -0400

    cleaner code : )

--- javascript.js
+++ javascript.js
@@ -1,80 +1,28 @@
-function infoToggle() {
-    var x = document.getElementById("homepage");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-    var x = document.getElementById("infoTab");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("detailTab");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("guidelinesTab");
-      if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "none";
-        }
-  }
 
-  function callToggle() {
-    var x = document.getElementById("homepage");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-    var x = document.getElementById("detailTab");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("infoTab");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("guidelinesTab");
-      if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "none";
-        }
-  }
+  $("#info").bind("click", function() {
+    $("#infoTab").show();
+    $("#homepage").hide();
+    $("#guidelinesTab").hide();
+    $("#detailsTab").hide();
+ });
 
-  function guidelinesToggle() {
-    var x = document.getElementById("homepage");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "none";
-    }
-    var x = document.getElementById("guidelinesTab");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("infoTab");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "none";
-      }
-      var x = document.getElementById("detailTab");
-      if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "none";
-        }
-  }
+ $("#guidelines").bind("click", function() {
+  $("#guidelinesTab").show();
+  $("#homepage").hide();
+  $("#infoTab").hide();
+  $("#detailsTab").hide();
+});
+
+$("#details").bind("click", function() {
+  $("#detailsTab").show();
+  $("#homepage").hide();
+  $("#guidelinesTab").hide();
+  $("#infoTab").hide();
+});
+
+$("#home").bind("click", function() {
+  $("#homepage").show();
+  $("#detailsTab").hide();
+  $("#guidelinesTab").hide();
+  $("#infoTab").hide();
+});
